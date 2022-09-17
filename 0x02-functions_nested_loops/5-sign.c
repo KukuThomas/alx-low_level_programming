80e1fdfc2b58b809b18211826fd6a8019009586c
KukuThomas <thomaskuk.net@gmail.com>
2022-09-17 00:55:33 -0700

    6-abs.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,57 +1,29 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * print_sign - Check code
- *
- *   * @n: An input number
- *
- *    *  Return: 0
- *
- *     */
-
+ * print_sign - Check code
+ * @n: An input number
+ * Return: 0
+ */
 int print_sign(int n)
-
 {
+	int value;
 
-		int value;
+	if (n > 0)
+	{
+		value = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		value = 0;
+		_putchar('0');
+	}
+	else
+	{
+		value = -1;
+		_putchar('-');
+	}
 
-
-
-			if (n > 0)
-
-					{
-
-								value = 1;
-
-										_putchar('+');
-
-											}
-
-				else if (n == 0)
-
-						{
-
-									value = 0;
-
-											_putchar('0');
-
-												}
-
-					else
-
-							{
-
-										value = -1;
-
-												_putchar('-');
-
-													}
-
-
-
-						return (value);
-
+	return (value);
 }
